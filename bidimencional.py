412a2ec153487c80388ab52d1d5d6aac99e79a9c
NaRubilar <na.rubilar@duocuc.cl>
2022-06-06 10:35:59 -0400

    waaaa

---
+++ bidimencional.py
@@ -0,0 +1,20 @@
+import numpy as np
+import random
+
+#matriz 3x3 
+matriz = np.diag([1,1,1])
+
+print("------------------------------------")
+
+#recorrer matriz
+for i in range(3):
+    for j in range(3):
+        print(matriz[i][j])
+
+print("------------------------------------")   
+        
+#Llenar num random
+for i in range(3):
+    for j in range(3):
+        matriz[i][j] = random.randint(0,100)
+print(matriz)
